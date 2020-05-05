1f5582d391c1f9353c91cd27e744a64053856e81
kibuikaCodes <kibuikasteve@gmail.com>
2020-05-05 13:00:26 +0300

    removed the image from main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
 // import "./index.css";
 const IndexPage = () => (
@@ -17,7 +16,6 @@ const IndexPage = () => (
             margin: "0 auto 1.45rem"
           }}
         >
-          <Image />
         </div>
       </div>
       <Link to="/blog/">View all posts</Link>
